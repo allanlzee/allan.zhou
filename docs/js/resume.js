0f11f66fa48e6a6095282b72d849769551118f2a
Allan Zhou <allanlinzhou@gmail.com>
2023-04-19 14:58:03 -0400

    Fixed js functions

--- docs/js/resume.js
+++ docs/js/resume.js
@@ -1,14 +1,16 @@
-var numberClicks = [0, 0, 0];
+var numberClicks = [0, 0, 0, 0];
 
 /**
  * Shows desired resume list and hides others. 
  * 
  * @param {*} listNum 
  */
-function showList(listNum) {
+function showList(listNum) { 
     let resumeLists = document.getElementsByClassName("resume-list"); 
 
-    numberClicks[listNum]++;
+    if (listNum != -1) {
+        numberClicks[listNum]++;
+    }
 
     for (i = 0; i < resumeLists.length; i++) {
         if (numberClicks[i] % 2 == 1) {
